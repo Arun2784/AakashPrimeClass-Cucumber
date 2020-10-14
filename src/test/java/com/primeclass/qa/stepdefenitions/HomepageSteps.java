f64ecd82ac0d0a4db349736f77d16e026b3c61a6
Arun Kumar Pandey <010645@Corpdomain.com>
2020-10-14 12:48:21 +0530

    small changes in step deffintion file

--- src/test/java/com/primeclass/qa/stepdefenitions/HomepageSteps.java
+++ src/test/java/com/primeclass/qa/stepdefenitions/HomepageSteps.java
@@ -14,26 +14,22 @@ public class HomepageSteps {
 
 	@When("user first click on login icon")
 	public void user_first_click_on_login_icon() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+	   
 	}
 
 	@Then("user reach on login page")
 	public void user_reach_on_login_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+	    
 	}
 
 	@Then("user now enter the Id and password")
 	public void user_now_enter_the_Id_and_password() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+	   
 	}
 
 	@Then("user click on login button")
 	public void user_click_on_login_button() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+	   
 	}
 
 }
